40ed6380554989f2ca391bd3a6f7f28fb0f90c23
njaip <njaip@ipst.ac.th>
2022-04-19 21:32:14 +0700

    อัพเดท interact

--- src/components/HomeSc2.js
+++ src/components/HomeSc2.js
@@ -1,13 +1,29 @@
 import  {Form , Button, Card,Container} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./HomeSc2.css"
-
+import {
+    galleryShop,
+    getPic,
+    balance,
+    pictures,
+  } from "../util/interact.js"
  
 
+
 function HomeSc2() {
-   const handleSubmit = (e) => {
+   const handleSubmit = async (e) => {
         e.preventDefault();
         console.log('Event: Form Submit');
+        const b = await balance();
+    console.log(b);
+    console.log("Test");
+    const pic = await getPic();
+    const picName = pic[1];
+    
+    console.log(pic);
+    console.log(picName);
+    const pictures1 = await pictures(1);
+    console.log(pictures1);
      };
   
    
@@ -22,7 +38,7 @@ function HomeSc2() {
             <Form.Control type="text" placeholder="id" />
             <Button variant="primary" type="submit"> ค้นหา </Button>
         </Form.Group>
-        <Form.Label>สถานะการจัดส่ง: กำลังจัดส่ง</Form.Label>
+        <Form.Label>สถานะการจัดส่ง: </Form.Label>
         </Form>
     </Card.Body>
     </Card>
---
+++ src/util/interact.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+require("dotenv").config();
+
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x04F4AD90b718F3b907D9bCa340d44640021aD541";
+
+export const galleryShop = new web3.eth.Contract(contractABI, contractAddress);
+
+export const getPic = async () => {
+  const pic = await galleryShop.methods.getPic().call();
+  return pic;
+};
+
+export const balance = async () => {
+  const balance = await web3.eth.getBalance(contractAddress);
+  return balance;
+};
+
+export const pictures = async (id) => {
+  const pictures = await galleryShop.methods.pictures(id).call();
+  return pictures;
+};

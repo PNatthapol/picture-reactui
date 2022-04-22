ab9c3137dd677a9348cd5e39ba77edf67347af6a
Babe <piya059@gmail.com>
2022-04-23 00:07:31 +0700

    :fire: comment new deploy

--- src/util/interact.js
+++ src/util/interact.js
@@ -1,5 +1,3 @@
-
-
 require("dotenv").config();
 
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
@@ -7,7 +5,7 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
 const contractABI = require("../contract-abi.json");
-const contractAddress = "0x04F4AD90b718F3b907D9bCa340d44640021aD541";
+const contractAddress = "0x04F4AD90b718F3b907D9bCa340d44640021aD541"; //0xE0833cE71df82277B5B47aaf289182d82721682c
 
 export const galleryShop = new web3.eth.Contract(contractABI, contractAddress);
 
@@ -33,20 +31,17 @@ export const connectWallet = async () => {
         method: "eth_requestAccounts",
       });
       const obj = {
-        
         address: addressArray[0],
       };
       return obj;
     } catch (err) {
       return {
         address: "",
-       
       };
     }
   } else {
     return {
-      address: ""
-      
+      address: "",
     };
   }
 };

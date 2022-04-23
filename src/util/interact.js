6d46d9660f8d9cabdbb64a620d978623eb3a1892
Babe <piya059@gmail.com>
2022-04-23 09:42:35 +0700

    :rocket: new contract

--- src/util/interact.js
+++ src/util/interact.js
@@ -5,7 +5,7 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
 const contractABI = require("../contract-abi.json");
-const contractAddress = "0x04F4AD90b718F3b907D9bCa340d44640021aD541"; //0xE0833cE71df82277B5B47aaf289182d82721682c
+const contractAddress = "0xE0833cE71df82277B5B47aaf289182d82721682c"; // 0x04F4AD90b718F3b907D9bCa340d44640021aD541
 
 export const galleryShop = new web3.eth.Contract(contractABI, contractAddress);
 
